10be52f8b003b02f84da464212ef515987f1d91f
coconut007-creator <1344421089@qq.com>
2020-07-18 11:29:43 +0800

    7.18

---
+++ alien_invasion/alien_invasion.py
@@ -0,0 +1,26 @@
+import sys
+import pygame
+
+def run_game():
+    #初始化游戏，创建一个屏幕对象
+    pygame.init()
+    screen = pygame.display.set_mode((1200, 600))
+    pygame.display.set_caption("Alien Invasion")
+    #设置背景颜色
+    bg_color = (230, 230, 230)
+
+    #开始游戏的主循环
+    while True:
+
+        #监视键盘和鼠标事件
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        #每次循环重新绘制屏幕
+        screen.fill(bg_color)
+
+        #让最近绘制的屏幕可见
+        pygame.display.flip()
+
+run_game()
---
+++ alien_invasion/settings.py
@@ -0,0 +1,6 @@
+class Setting():
+    #储存所有设置的类
+    def __init__(self):
+        self.screen_width=1200
+        self.screen_length=600
+        self.bg_color=(230, 230, 230)
