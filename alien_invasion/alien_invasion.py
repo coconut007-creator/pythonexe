b18311de465c1d6dec129e846d6dbe2907c7c0b8
coconut007-creator <1344421089@qq.com>
2020-07-18 15:21:55 +0800

    rocket_game_7_18

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,26 +1,26 @@
 import sys
 import pygame
-
+from settings import Setting
+from ship import Ship
+import game_functions as gf
 def run_game():
     #初始化游戏，创建一个屏幕对象
     pygame.init()
-    screen = pygame.display.set_mode((1200, 600))
+    ai_settings = Setting()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_length))
     pygame.display.set_caption("Alien Invasion")
     #设置背景颜色
     bg_color = (230, 230, 230)
+    ship = Ship(ai_settings, screen)
 
     #开始游戏的主循环
     while True:
 
         #监视键盘和鼠标事件
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
+        gf.check_events(ship)
+        ship.update()
         #每次循环重新绘制屏幕
-        screen.fill(bg_color)
-
         #让最近绘制的屏幕可见
-        pygame.display.flip()
+        gf.update_screen(ai_settings, screen, ship)
 
 run_game()
--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -1,6 +1,7 @@
-class Setting():
+class Setting:
     #储存所有设置的类
     def __init__(self):
         self.screen_width=1200
         self.screen_length=600
         self.bg_color=(230, 230, 230)
+        self.ship_speed_factor = 1.5
